8e53c766013c8addc8ebf305c036ad34a43353f1
Dhiogo Corrêa <dhiogo.sa@usp.br>
2018-10-23 12:44:23 -0300

    fix headers

--- src/__init__.py
+++ src/__init__.py
@@ -13,7 +13,7 @@ except OSError:
 states = ['AC','AL','AP','AM','BA','CE','DF','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO']
 
 f = open(outputFile, 'a')
-f.write('Cidade,Estado,Mínimo Temperatura,Máximo Temperatura,Precipitação (mm)\n')
+f.write('Cidade,Estado,Mês,Mínimo Temperatura,Máximo Temperatura,Precipitação (mm)\n')
 f.close()
 
 def crawl():
